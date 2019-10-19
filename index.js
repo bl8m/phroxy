4f4c3bae2f076fc898404e501ac85f0d901909a7
Massimiliano Brunazzi <m.brunazzi@bloomdesign.it>
2019-10-19 21:36:53 +0100

    Integrato il db

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let PhoneManager = require('./lib/PhoneManager.js');
 let TellowsConnector = require('./lib/TellowsConnector.js');
 
-//let pm = new PhoneManager('/dev/ttyACM0');
-let tc = new TellowsConnector();
 
-tc.check('0321465872').then(function(result){
-	console.log(result);
-});
+let pm = new PhoneManager('/dev/ttyACM0', './phroxy.sqlite');
+
+pm.on('ring', () => { console.log('RING') });
+pm.on('call-received', (number) => { console.log('RECEIVED ' + number) });
+pm.on('call-rejected', (number) => { console.log('REJECTED ' + number) });
+// let tc = new TellowsConnector();
+
+// tc.check('0321465872').then(function(result){
+// 	console.log(result);
+// });
+
 
 
 
